dc229c61f725b39b5b14582c7b2785c9a4f0e9e0
Sazib <mahiburislamsazib@gmail.com>
2024-07-04 11:31:08 +0600

    array.js

---
+++ array.js
@@ -0,0 +1,27 @@
+// How to declare an array in JS
+
+var cars = ["Saab", "Volvo", "BMW"];
+
+// Number of elements in an Array
+
+var myArray = [11, 22, 44, 88, 99, 32];
+var total = myArray.length;
+myArray.length =3;
+console.log(myArray);
+
+// Find the value of an element by index
+
+var valueIndex = myValues[1];
+
+//Change an element by index
+
+var fruits =['apple','banana','orange'];
+var item = fruits.indexOf("banana");
+fruits[1] ="mango";
+console.log(fruits);
+
+
+//get the index of an element by the value
+var fruitPriceList = [100, 200, 300, 400, 500];
+var index = fruitPriceList.indexOf(200);
+
