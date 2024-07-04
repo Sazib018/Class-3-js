a47a3bca8ab2e76adf8d7b62cece1fc1c5dc3a35
Sazib <mahiburislamsazib@gmail.com>
2024-07-04 13:28:38 +0600

    loop

---
+++ loop.js
@@ -0,0 +1,34 @@
+// Loop Task
+
+// Display "Ajke amar mon valo nei" for 39 times
+
+var repeatedItem = "ajke amar mon valo nei".repeat(39);
+console.log(repeatedItem);
+
+// Display numbers between 58 to 98
+
+var number = 58;
+while (number <= 98) {
+    console.log(number);
+    number++;
+}
+
+// Show all even numbers from 412 to 456
+
+for (let i = 412; i <= 456; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+
+// Show all odd numbers 581 to 623
+
+var number = 581;
+while (number <= 623) {
+    if (number % 2 !== 0) {
+        console.log(number);
+    }
+    number++;
+}
+
+// 
