9390c7931cd16bf87d25929198078082b2ef99aa
Sazib <mahiburislamsazib@gmail.com>
2024-07-04 14:48:05 +0600

    all done

--- loop.js
+++ loop.js
@@ -31,4 +31,30 @@ while (number <= 623) {
     number++;
 }
 
-// 
+// Declare an array for all the topics that you have learned last few days display then as output
+
+var lernFewdays =["array","push","pop","update","loop","else","else if","if","function","all object",];
+
+// Create an array for all the mobile brands. Display all the elements of the array by using a while loop
+
+var mobileBrands = ["Samsung", "Apple", "Google", "OnePlus", "Xiaomi"];
+
+var i = 0;
+while (i < mobileBrands.length) {
+    console.log(mobileBrands[i]);
+    i++;
+}
+
+// Run a loop from 30 to 86. this loop will stop if the values get higher than 44
+
+for (var i = 30; i <= 86; i++) {
+    if (i > 44) {
+        break;
+    }
+    console.log(i);
+}
+
+// Write the price of the books that you have. Display the prices if the prices is lower than 200
+
+
+
