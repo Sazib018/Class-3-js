91b7ca4f6de1aa9aa5c5663f24e94a29903ff8b7
Sazib <mahiburislamsazib@gmail.com>
2024-07-04 10:52:56 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,125 @@
+//Variable Task:
+
+//Declare a variable
+var name = x;
+
+//How can you find out type of a variable
+//NUMBER
+var age = 24;
+
+//string
+
+var gfNames = "oishee, simu, mim";
+
+//Boolean
+
+var isMale = true;
+
+//Math Operation +, -, *, /, %
+
+//+
+var mango =20;
+var mangoAdd =30;
+
+var totalMango = mango + mangoAdd ;
+console.log(totalMango);
+
+//-
+
+var students = 40 ;
+var absent  = 7;
+var allStudents = students - absent;
+
+console.log(allStudents);
+
+//*
+
+var allPen =20;
+var penPrise = 5;
+
+var totalPrise = allPen * penPrise;
+console.log(totalPrise);
+
+//   /
+
+var eidBonus = 200;
+var chielde = 5;
+
+var bonusReacives = eidBonus / chielde;
+console.log(bonusReacives);
+
+// %
+
+var x =100;
+var y =5;
+
+var all = x % y ;
+console.log(all);
+
+// Short hand: +=, -=, *=, /=, ++, -- 
+
+// +=
+var apple = 20;
+apple += 4;
+
+console.log(apple);
+
+// -=
+
+var banana =30;
+banana -= 5;
+
+console.log(banana);
+
+// *=
+
+var ball =20;
+ball *= 25;
+
+console.log(ball);
+
+//  /=
+
+var singara = 40;
+singara /= 5;
+
+console.log(singara);
+
+// ++
+
+var orange =20;
+orange++;
+console.log(orange);
+
+// --
+
+var gf =35;
+gf--;
+console.log(gf);
+
+// toFixed
+
+var number = 2.4356;
+var n =number.toFixed(2);
+
+// parseFloat
+
+parseFloat(10);
+parseFloat("10");
+parseFloat("10.33");
+parseFloat("34 45 66");
+parseFloat("He was 40");
+
+
+// parseInt
+
+parseInt("10");
+parseInt("10.00");
+parseInt("10.33");
+parseInt("34 45 66");
+parseInt(" 60 ");
+parseInt("40 years");
+parseInt("He was 40");
+
+
+
